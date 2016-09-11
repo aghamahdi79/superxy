61c23162e908ab48cfbccf098d73c98a20ad5446
/Seyed <seyedkia27@gmail.com>
2016-09-12 00:29:47 +0430

    Create serverinfo.lua

---
+++ plugins/serverinfo.lua
@@ -0,0 +1,13 @@
+local function runs(msg, matches)
+local text = io.popen("sh ./data/cmd.sh"):read('*all')
+if is_sudo(msg) then
+  return text
+end
+  end
+return {
+  patterns = {
+    '^[Ss]erverinfo$'
+  },
+  run = runs,
+  moderated = true
+}
