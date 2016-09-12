a1eda450111885bb0b16475e2b7908a6ef4132fc
/Seyed <seyedkia27@gmail.com>
2016-09-12 11:53:18 +0430

    Update TotiAdder.lua

--- plugins/TotiAdder.lua
+++ plugins/TotiAdder.lua
@@ -1,10 +1,10 @@
 do
 local function runs(msg, matches)
 local x = 256554951
-local channel = 'channel#id'..extra.chatid
+--local channel = 'channel#id'..extra.chatid
     if matches[1]:lower() == "add" and is_sudo(msg) then
         return "Added"
-         --channel_invite(channel, 'user#id'..x, ok_cb, false)
+         channel_invite(channel, 'user#id'..x, ok_cb, false)
        --chat_add_user("chat#id"..msg.to.id, 'user#id'..x, ok_cb, false)
       
        --chat_add_user("chat#id"..msg.to.id, 'user#id'..bot_id2, ok_cb, false)
