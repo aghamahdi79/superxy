513d3cc9f124958e305819a0c23d112b2e15bcfb
/Seyed <seyedkia27@gmail.com>
2016-09-12 11:39:03 +0430

    Update TotiAdder.lua

--- plugins/TotiAdder.lua
+++ plugins/TotiAdder.lua
@@ -1,29 +1,10 @@
 do
 local function runs(msg, matches)
-local x = 114760915
-local y = 245379144
-local x = 114760915
-local y = 245379144
-local x = 114760915
-local y = 245379144
-local x = 114760915
-local y = 245379144
-local x = 114760915
-local y = 245379144
-local x = 114760915
-local y = 245379144
-local x = 114760915
-local y = 245379144
-local x = 114760915
-local y = 245379144
-local x = 114760915
-local y = 245379144
-local x = 114760915
-local y = 245379144
+local x = 256554951
 
     if matches[1]:lower() == "add" and is_sudo(msg) then
-       chat_add_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
-       chat_add_user("chat#id"..msg.to.id, 'user#id'..bot_id2, ok_cb, false)
+       chat_add_user("chat#id"..msg.to.id, 'user#id'..x, ok_cb, false)
+       --chat_add_user("chat#id"..msg.to.id, 'user#id'..bot_id2, ok_cb, false)
        return "Added"
     end
 end
